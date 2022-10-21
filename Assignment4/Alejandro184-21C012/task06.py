94a9a5a70668a4f1fd7647768bbde60b1a43510b
Alejandro184 <93882567+Alejandro184@users.noreply.github.com>
2022-10-21 23:43:57 +0200

    Update task06.py

--- Assignment4/Alejandro184-21C012/task06.py
+++ Assignment4/Alejandro184-21C012/task06.py
@@ -60,6 +60,6 @@ for s, p, o in g:
 """**TASK 6.5: Add UPM as the university where John Smith works**"""
 
 g.add((ns.UPM, RDF.type, ns.University))
-g.add((ns.JohnSmith, vcard.Work, ns.UPM))
+g.add((ns.JohnSmith, ns.Workplace, ns.UPM))
 for s, p, o in g:
   print(s,p,o)
