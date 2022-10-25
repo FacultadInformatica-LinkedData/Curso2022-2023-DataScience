043b31a3ac977a4f4393eadc1942be83adb8a592
davidhernandog <113912014+davidhernandog@users.noreply.github.com>
2022-10-25 12:35:34 +0200

    Update task06.py

--- Assignment4/davidhernandog_D21C027/task06.py
+++ Assignment4/davidhernandog_D21C027/task06.py
@@ -60,6 +60,6 @@ for s, p, o in g:
 """**TASK 6.5: Add UPM as the university where John Smith works**"""
 
 g.add((ns.UPM, RDF.type, ns.University))
-g.add((ns.UPM, vcard.member, ns.JohnSmith))
+g.add((ns.UPM, vcard.worksAt, ns.JohnSmith))
 for s, p, o in g:
   print(s,p,o)
