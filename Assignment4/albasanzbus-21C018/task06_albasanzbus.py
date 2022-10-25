49164df5a890bb91aff91b45288ff6d0e44b8f89
albasanzbus <113853896+albasanzbus@users.noreply.github.com>
2022-10-25 21:32:20 +0200

    Assignment 4 corregido

--- Assignment4/albasanzbus-21C018/task06_albasanzbus.py
+++ Assignment4/albasanzbus-21C018/task06_albasanzbus.py
@@ -9,7 +9,6 @@ Original file is located at
 **Task 06: Modifying RDF(s)**
 """
 
-!pip install rdflib 
 github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2021-2022/master/Assignment4/course_materials"
 
 """Leemos el fichero RDF de la forma que lo hemos venido haciendo"""
@@ -62,7 +61,7 @@ for s, p, o in g.triples((ns.JaneSmith, None, None)):
 
 VCARD = Namespace("http://www.w3.org/2001/vcard-rdf/3.0#")
 g.add((ns.UPM, RDF.type, ns.University))
-g.add((ns.JohnSmith, ns.worksAT, ns.UPM))
+g.add((ns.JohnSmith, ns.worksAt, ns.UPM))
 
 for s, p, o in g.triples((ns.JohnSmith, None, None)):
   print(s,p,o)
